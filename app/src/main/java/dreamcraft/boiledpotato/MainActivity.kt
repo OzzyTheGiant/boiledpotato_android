bc3c027055b3d4062be02bc6998542bba9519672
Oziel Perez <OZLperez11@gmail.com>
2019-07-02 00:58:31 -0500

    Refactored cuisine buttons to use only one click listener

--- app/src/main/java/dreamcraft/boiledpotato/MainActivity.kt
+++ app/src/main/java/dreamcraft/boiledpotato/MainActivity.kt
@@ -3,37 +3,43 @@ package dreamcraft.boiledpotato
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.content.Intent
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toast_error.view.*
 
 object GLOBALS {
-    const val EXTRAS_SEARCH = "SEARCH_RESULT"
+    const val EXTRAS_SEARCH = "SEARCH_QUERY"
+    const val EXTRAS_CUISINE = "CUISINE"
     const val TOAST_MESSAGE = "Please enter an ingredient"
 }
 
 class MainActivity : AppCompatActivity() {
+    private val searchButtonClickListener = fun(_: View) = startSearchResultsActivity(search_field.text.toString())
+    private val cuisineButtonClickListener = fun(view: View) = startSearchResultsActivity((view as Button).text.toString())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        search_button.setOnClickListener { startSearchResultsActivity(search_field.text) }
-        cuisine_button_american.setOnClickListener { startSearchResultsActivity(cuisine_button_american.text) }
-        cuisine_button_mexican.setOnClickListener { startSearchResultsActivity(cuisine_button_mexican.text) }
-        cuisine_button_chinese.setOnClickListener { startSearchResultsActivity(cuisine_button_chinese.text) }
-        cuisine_button_japanese.setOnClickListener { startSearchResultsActivity(cuisine_button_japanese.text) }
-        cuisine_button_indian.setOnClickListener { startSearchResultsActivity(cuisine_button_indian.text) }
-        cuisine_button_french.setOnClickListener { startSearchResultsActivity(cuisine_button_french.text) }
-        cuisine_button_italian.setOnClickListener { startSearchResultsActivity(cuisine_button_italian.text) }
-        cuisine_button_spanish.setOnClickListener { startSearchResultsActivity(cuisine_button_spanish.text) }
-        cuisine_button_british.setOnClickListener { startSearchResultsActivity(cuisine_button_british.text) }
+        search_button.setOnClickListener(searchButtonClickListener)
+        cuisine_button_american.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_mexican.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_chinese.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_japanese.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_indian.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_french.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_italian.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_spanish.setOnClickListener(cuisineButtonClickListener)
+        cuisine_button_british.setOnClickListener(cuisineButtonClickListener)
     }
 
     /** Initiate search results activity by passing search keyword to be used in Spoonacular API */
     @SuppressLint("InflateParams")
-    private fun startSearchResultsActivity(text: CharSequence) {
-        if (text.toString() == "") {
+    private fun startSearchResultsActivity (text: String) {
+        if (text == "") {
             val toast = Toast(applicationContext)
             toast.view = layoutInflater.inflate(R.layout.toast_error, null)
             toast.view.toast_error_message.text = GLOBALS.TOAST_MESSAGE
