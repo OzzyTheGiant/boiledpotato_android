544eb17509abc0dfcb0ea80bdc9ddbc3460c5ce7
Oziel Perez <OZLperez11@gmail.com>
2019-07-13 00:15:13 -0500

    Created custom app class for supporting dependency injection

---
+++ app/src/main/java/dreamcraft/boiledpotato/AndroidApplication.kt
@@ -0,0 +1,19 @@
+package dreamcraft.boiledpotato
+
+import android.app.Application
+import dreamcraft.boiledpotato.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class AndroidApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@AndroidApplication)
+            modules(appModule)
+        }
+    }
+}
--- app/src/main/java/dreamcraft/boiledpotato/viewmodels/SearchResultsViewModel.kt
+++ app/src/main/java/dreamcraft/boiledpotato/viewmodels/SearchResultsViewModel.kt
@@ -4,16 +4,26 @@ import android.util.SparseArray
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dreamcraft.boiledpotato.models.Recipe
+import dreamcraft.boiledpotato.repositories.RecipeRepository
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import org.koin.core.qualifier.named
 
-class SearchResultsViewModel : ViewModel() {
-    var recipeList = MutableLiveData<SparseArray<Recipe>>()
+class SearchResultsViewModel : ViewModel(), KoinComponent {
+    private val repository : RecipeRepository by inject()
+    private val webApiResultsSize: Int by inject(named("webApiResultsSize"))
+    public val recipesContainer = MutableLiveData<SparseArray<Recipe>>()
 
-    init {
-        val recipe = Recipe(1, "Chicken Soup", 30, 4, "soup.jpg")
-        val recipes = SparseArray<Recipe>()
-        for (i in 0..20) {
-            recipes.append(i, recipe)
+    init { recipesContainer.value = SparseArray() }
+
+    fun getRecipes(searchKeywords: String, cuisine: String) {
+        repository.searchRecipes(searchKeywords, cuisine, webApiResultsSize) { response: SparseArray<Recipe> ->
+            for (i in 0 until response.size()) {
+                recipesContainer.value?.let {
+                    it.append(it.size(), response.valueAt(i))
+                }
+            }
+            recipesContainer.value = recipesContainer.value // trigger observer notification
         }
-        recipeList.value = recipes
     }
 }
