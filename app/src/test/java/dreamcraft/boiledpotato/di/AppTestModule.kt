66c8de33f70d527d19aac6845e19e5fab38e6adc
Oziel Perez <OZLperez11@gmail.com>
2019-09-30 00:04:22 -0500

    Added a Recipe to appTestModule to use in stubs and as sample data

--- app/src/test/java/dreamcraft/boiledpotato/di/AppTestModule.kt
+++ app/src/test/java/dreamcraft/boiledpotato/di/AppTestModule.kt
@@ -1,21 +1,49 @@
 package dreamcraft.boiledpotato.di
 
 import dreamcraft.boiledpotato.models.Recipe
+import dreamcraft.boiledpotato.utilities.CoroutineContextProvider
+import dreamcraft.boiledpotato.utilities.TestCoroutineContextProvider
 import dreamcraft.boiledpotato.viewmodels.RecipeViewModel
 import dreamcraft.boiledpotato.viewmodels.RecipeViewModelStub
 import dreamcraft.boiledpotato.viewmodels.SearchResultsViewModel
 import dreamcraft.boiledpotato.viewmodels.SearchResultsViewModelStub
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val appTestModule : Module = module(override = true) {
     viewModel<SearchResultsViewModel> { SearchResultsViewModelStub() }
     viewModel<RecipeViewModel> { RecipeViewModelStub() }
+    single<CoroutineContextProvider> { TestCoroutineContextProvider() }
 
     single<List<Recipe>> {
         val recipes = ArrayList<Recipe>(10)
         for (i in 0..9) recipes.add(i, Recipe(i.toLong(), "Chicken Soup $i", 60, "chicken-soup.jpg"))
         recipes
+    }
+
+    single(named("ingredients")) {
+        listOf(
+            "2 cups of Chicken Broth",
+            "1/2 lb of Chicken Breast",
+            "1 pack of noodles"
+        )
+    }
+
+    single(named("instructions")) {
+        listOf(
+            "Grill chicken breast in pan",
+            "Boil noodles in chicken broth",
+            "Add chicken to soup",
+            "Serve"
+        )
+    }
+
+    single {
+        val recipe = Recipe(1, "Chicken Soup", 60, "chicken-soup.jpg")
+        recipe.ingredients = get(named("ingredients"))
+        recipe.instructions = get(named("instructions"))
+        recipe
     }
 }
--- app/src/test/java/dreamcraft/boiledpotato/viewmodels/RecipeViewModelStub.kt
+++ app/src/test/java/dreamcraft/boiledpotato/viewmodels/RecipeViewModelStub.kt
@@ -1,21 +1,20 @@
 package dreamcraft.boiledpotato.viewmodels
 
+import org.koin.core.inject
+import org.koin.core.qualifier.named
+
 class RecipeViewModelStub : RecipeViewModel() {
+    private val testIngredients : List<String> by inject(named("ingredients"))
+    private val testInstructions : List<String> by inject(named("instructions"))
+
     var getRecipeDetailsCalls = 0
     var checkIfRecipeIsFavoriteCalls = 0
     var toggleRecipeAsFavoriteCalls = 0
 
     override fun getRecipeDetails() {
         getRecipeDetailsCalls++
-        recipe.ingredients = ArrayList()
-        (recipe.ingredients as ArrayList<String>).add("2 cups of Chicken Broth")
-        (recipe.ingredients as ArrayList<String>).add("1/2 lb of Chicken Breast")
-        (recipe.ingredients as ArrayList<String>).add("1 pack of noodles")
-        recipe.instructions = ArrayList()
-        (recipe.instructions as ArrayList<String>).add("Grill chicken breast in pan")
-        (recipe.instructions as ArrayList<String>).add("Boil noodles in chicken broth")
-        (recipe.instructions as ArrayList<String>).add("Add chicken to soup")
-        (recipe.instructions as ArrayList<String>).add("Serve")
+        recipe.ingredients = testIngredients
+        recipe.instructions = testInstructions
     }
 
     override fun checkIfRecipeIsFavorite() {
