2f9ca4389f76eda44d8e6d21f9a1966d059daf15
Oziel Perez <OZLperez11@gmail.com>
2019-10-01 09:49:26 -0500

    Removed RecipeViewModelStub dependency to use a mock instead

--- app/src/test/java/dreamcraft/boiledpotato/activities/RecipeActivityUnitTest.kt
+++ app/src/test/java/dreamcraft/boiledpotato/activities/RecipeActivityUnitTest.kt
@@ -2,6 +2,7 @@ package dreamcraft.boiledpotato.activities
 
 import android.content.Intent
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
@@ -9,21 +10,21 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import dreamcraft.boiledpotato.R
 import dreamcraft.boiledpotato.RobolectricApplication
-import dreamcraft.boiledpotato.di.appTestModule
 import dreamcraft.boiledpotato.models.Recipe
-import dreamcraft.boiledpotato.repositories.RecipeRepository
 import dreamcraft.boiledpotato.repositories.Resource
-import dreamcraft.boiledpotato.viewmodels.RecipeViewModelStub
+import dreamcraft.boiledpotato.viewmodels.RecipeViewModel
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.core.context.loadKoinModules
 import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
 import org.koin.test.mock.declareMock
@@ -32,24 +33,30 @@ import org.koin.test.mock.declareMock
 class RecipeActivityUnitTest : KoinTest {
     private val app = getApplicationContext<RobolectricApplication>()
     private lateinit var scenario: ActivityScenario<RecipeActivity>
-    private val recipe = Recipe(1L, "Chicken Soup", 60, "chicken-soup.jpg")
+    private val sampleRecipe = Recipe(1L, "Chicken Soup", 60, "chicken-soup.jpg")
 
     @get:Rule val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before fun setUp() {
         val intent = Intent(app, RecipeActivity::class.java)
-        intent.putExtra(IntentExtras.RECIPE, recipe)
-        loadKoinModules(appTestModule)
-        declareMock<RecipeRepository>()
+        intent.putExtra(IntentExtras.RECIPE, sampleRecipe)
+
+        declareMock<RecipeViewModel> {
+            // stub properties (you can do this as they have getters and setters)
+            whenever(recipeLiveData).doReturn(MutableLiveData())
+            whenever(favoriteLiveData).doReturn(MutableLiveData())
+            whenever(recipe).doReturn(sampleRecipe)
+        }
+
         scenario = ActivityScenario.launch(intent)
     }
 
     @Test fun displaysProvidedRecipeDataOnStartUp() {
 
         // check parcel Recipe data came in and filled UI
-        onView(withId(R.id.recipe_name)).check(matches(withText(recipe.name)))
-        onView(withId(R.id.servings)).check(matches(withText("${recipe.servings} Servings")))
-        onView(withId(R.id.prep_time)).check(matches(withText("${recipe.prepMinutes} min")))
+        onView(withId(R.id.recipe_name)).check(matches(withText(sampleRecipe.name)))
+        onView(withId(R.id.servings)).check(matches(withText("${sampleRecipe.servings} Servings")))
+        onView(withId(R.id.prep_time)).check(matches(withText("${sampleRecipe.prepMinutes} min")))
 
         // check ingredients and instructions placeholders are visible
         onView(withId(R.id.ingredients_placeholder)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
@@ -58,13 +65,8 @@ class RecipeActivityUnitTest : KoinTest {
 
     @Test fun displaysIngredientsAndInstructionsAfterFetchingData() {
         scenario.onActivity { activity ->
-            val viewModel = activity.viewModel as RecipeViewModelStub
-            val resource = Resource.Success(recipe)
+            val resource = Resource.Success(sampleRecipe)
             activity.processRecipeResource(resource)
-
-            // check that repository methods were called
-            assertEquals(viewModel.getRecipeDetailsCalls, 1)
-            assertEquals(viewModel.checkIfRecipeIsFavoriteCalls, 1)
 
             // check ingredients and instructions visible and placeholders are gone
             onView(withId(R.id.ingredients_list)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
@@ -79,21 +81,21 @@ class RecipeActivityUnitTest : KoinTest {
             activity.viewModel.recipe.instructions?.let { for (i in it.indices) {
                 onView(withId(R.id.instructions_list)).check(matches(withText(containsString(it[i]))))
             }}
+
+            // check that repository methods were called
+            verify(activity.viewModel, times(1)).getRecipeDetails()
+            verify(activity.viewModel, times(1)).getFavoriteStatus()
         }
     }
 
     @Test fun togglesFavoriteIcon() {
         scenario.onActivity { activity ->
-            val viewModel = activity.viewModel as RecipeViewModelStub
-
-            activity.processFavoriteResource(Resource.Success(true))
-            assert(viewModel.recipe.isFavorite) // then check favorite is true after querying
+            activity.processFavoriteResource(Resource.Success(false))
 
             // test favorite button toggles isFavorite in Recipe
             onView(withId(R.id.button_favorite)).perform(click())
-            assertEquals(viewModel.toggleRecipeAsFavoriteCalls, 1)
+            verify(activity.viewModel, times(1)).toggleFavoriteStatus()
             activity.processFavoriteResource(Resource.Success(false))
-            assert(!viewModel.recipe.isFavorite)
         }
     }
 
--- app/src/test/java/dreamcraft/boiledpotato/di/AppTestModule.kt
+++ app/src/test/java/dreamcraft/boiledpotato/di/AppTestModule.kt
@@ -3,8 +3,6 @@ package dreamcraft.boiledpotato.di
 import dreamcraft.boiledpotato.models.Recipe
 import dreamcraft.boiledpotato.utilities.CoroutineContextProvider
 import dreamcraft.boiledpotato.utilities.TestCoroutineContextProvider
-import dreamcraft.boiledpotato.viewmodels.RecipeViewModel
-import dreamcraft.boiledpotato.viewmodels.RecipeViewModelStub
 import dreamcraft.boiledpotato.viewmodels.SearchResultsViewModel
 import dreamcraft.boiledpotato.viewmodels.SearchResultsViewModelStub
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,7 +12,6 @@ import org.koin.dsl.module
 
 val appTestModule : Module = module(override = true) {
     viewModel<SearchResultsViewModel> { SearchResultsViewModelStub() }
-    viewModel<RecipeViewModel> { RecipeViewModelStub() }
     single<CoroutineContextProvider> { TestCoroutineContextProvider() }
 
     single<List<Recipe>> {
