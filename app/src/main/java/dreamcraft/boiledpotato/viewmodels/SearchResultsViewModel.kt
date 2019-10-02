8947cf4f9afba29f1b00f39ec91bc559f3166a63
Oziel Perez <OZLperez11@gmail.com>
2019-10-02 12:01:05 -0500

    Refactored SearchResultsViewModel to be more unit testable

--- app/src/main/java/dreamcraft/boiledpotato/viewmodels/SearchResultsViewModel.kt
+++ app/src/main/java/dreamcraft/boiledpotato/viewmodels/SearchResultsViewModel.kt
@@ -8,9 +8,9 @@ import dreamcraft.boiledpotato.models.Recipe
 import dreamcraft.boiledpotato.models.RecipeSearchQuery
 import dreamcraft.boiledpotato.repositories.RecipeRepository
 import dreamcraft.boiledpotato.repositories.Resource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import dreamcraft.boiledpotato.utilities.CoroutineContextProvider
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.core.qualifier.named
@@ -18,42 +18,45 @@ import org.koin.core.qualifier.named
 class SearchResultsViewModel : ViewModel(), KoinComponent {
     // dependencies
     private val repository : RecipeRepository by inject()
+    private val ctxProvider : CoroutineContextProvider by inject()
     private val maxResultsSize: Int by inject(named("webApiResultsSize"))
 
-    private lateinit var repositoryJob: Job
+    // parcel data
     lateinit var searchKeywords: String
     lateinit var cuisine: String
+
+    // Recipes data
     val resourceLiveData = MutableLiveData<Resource<RecipeSearchQuery>>()
     val recipes = SparseArray<Recipe>()
     var totalResults = 0
 
-    fun getRecipes() {
-        repositoryJob = viewModelScope.launch(Dispatchers.IO) {
-            val resource = if (searchKeywords != "favorites") {
-                repository.searchRecipes(searchKeywords, cuisine, maxResultsSize, recipes.size())
-            } else {
-                repository.getFavoriteRecipes(maxResultsSize, recipes.size(), totalResults == 0)
-            }
-
-            if (resource is Resource.Success && resource.data != null) {
-                if (totalResults == 0) totalResults = resource.data.totalResults
-
-                // append recipes from current search query to master recipe array
-                resource.data.recipes?.let { resultRecipes ->
-                    for (i in resultRecipes.indices) {
-                        recipes.append(recipes.size(), resultRecipes[i]) // size = new key
-                    }
+    fun fetchRecipes() {
+        viewModelScope.launch {
+            val resource = withContext(ctxProvider.IO) {
+                if (searchKeywords != "favorites") {
+                    repository.searchRecipes(searchKeywords, cuisine, maxResultsSize, recipes.size())
+                } else {
+                    repository.getFavoriteRecipes(maxResultsSize, recipes.size(), totalResults == 0)
                 }
             }
 
-            resourceLiveData.postValue(resource)  // set value async since operating on co-routine
+            updateRecipeArray(resource)
+            resourceLiveData.value = resource
         }
 
         resourceLiveData.value = Resource.Loading()
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        repositoryJob.cancel() // cancel DB/API call if in progress
+    fun updateRecipeArray(resource: Resource<RecipeSearchQuery>) {
+        if (resource is Resource.Success && resource.data != null) {
+            if (totalResults == 0) totalResults = resource.data.totalResults
+
+            // append recipes from current search query to master recipe array
+            resource.data.recipes?.let { resultRecipes ->
+                for (i in resultRecipes.indices) {
+                    recipes.append(recipes.size(), resultRecipes[i]) // size = new key
+                }
+            }
+        }
     }
 }
